1f74d46d9b2ba3d8b1e664dc9092a98f974c1dce
Zum139 <Zum139@mail.ru>
2022-09-29 22:23:54 +0700

    fix bags:)

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
 from PyQt5.QtCore import QCoreApplication
 import PyQt5
 import time
+import sip
 
 
 #Class the window my program
-class MainWindow(QWidget):
+class Example(QWidget):
+
 
     def __init__(self):
         super().__init__()
@@ -21,23 +23,17 @@ class MainWindow(QWidget):
         self.setWindowTitle('My program')
 
         qbtn = QPushButton('Remind', self)
-        qbtn.clicked.connect(RemindsWin())
+        qbtn.clicked.connect(self.RemindsWin)
         qbtn.resize(qbtn.sizeHint())
         qbtn.move(50, 50)
         self.show()
         #time.sleep(20)
         #exit(0)
 
-
-class RemindsWin(QWidget):
-
-    def __init__(self):
-        super().__init__()
-
-        self.initUI()
-
     #window for remind
-    def remindsWin(self):
+    def RemindsWin(self):
+
+
         self.setGeometry(300, 300, 350, 150)
         self.setWindowTitle('Second Window')
 
@@ -51,5 +47,5 @@ class RemindsWin(QWidget):
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
-    mw = MainWindow()
+    ex = Example()
     sys.exit(app.exec_())
