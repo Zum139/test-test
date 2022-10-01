1b87827baf782810b71208fd985b5eca827f8c1c
Zum139 <Zum139@mail.ru>
2022-10-01 17:17:33 +0700

    finally fix bags:)))))))

--- main.py
+++ main.py
@@ -1,25 +1,16 @@
-#cd .git/Projects/Lessons/1/5
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
 from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtWidgets  import (QWidget, QMessageBox, QApplication, QToolTip,
+    QPushButton)
 import PyQt5
 import time
-import sip
+import sys
 
 
 #Class the window my program
 class Example(QWidget):
 
-
     def __init__(self):
         super().__init__()
-
-        self.initUI()
-
-
-    def initUI(self):
-        self.w = QWidget()
-        self.setGeometry(300, 300, 250, 150)
         self.setWindowTitle('My program')
 
         qbtn = QPushButton('Remind', self)
@@ -32,16 +23,21 @@ class Example(QWidget):
 
     #window for remind
     def RemindsWin(self):
-
-
         self.setGeometry(300, 300, 350, 150)
         self.setWindowTitle('Second Window')
 
-        qbtn1 = QPushButton('Back', self)
-        qbtn1.resize(qbtn1.sizeHint())
-        qbtn1.clicked.connect(self.initUI)
-        qbtn1.move(50, 100)
-        qbtn1.show()
+
+class RemindsWin(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    #window for remind
+    def remindsWin(self):
+        self.setGeometry(300, 300, 350, 150)
+        self.setWindowTitle('Second Window')
 
 
 if __name__ == '__main__':
